7df4272f49711fdb963baa805dac1b933a91122e
NICOLAS MATEUS FRANCISCO <nicolas_francisco@estudante.sesisenai.org.br>
2024-09-24 09:38:21 -0300

    style: fazer tela de visualizar unico professor

--- views/visualizarProf.php
+++ views/visualizarProf.php
@@ -1,19 +1,35 @@
-<div>
-  <a href="index.php">Voltar</a>
-  
-  <form action="editar.php" method="POST">
-    <label for="nome">Foto</label>
-    <input type="file" name="file">
-  
-    <label for="nome">Nome</label>
-    <input type="text" name="nome">
+<head>
+  <link rel="stylesheet" href="../styles/visualizarProf.css">
+  <link rel="stylesheet" href="../styles/index.css">
+</head>
+<div id="visualizarProf">
+  <a href="index.php" id="voltarProf">Voltar</a>
+  <div id="centralizar">
+    <div id="informacaoProf">
+      <form action="editar.php" method="POST">
+        <div id="fotinha">
+          <label for="nome">Foto</label>
+          <input type="file" name="file">
+        </div>
+        <div id="infoPessoa">
+          <div>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome">
+          </div>
 
-    <label for="nome">Matéria</label>
-    <input type="text" name="materia">
+          <div>
+            <label for="nome">Matéria:</label>
+            <input type="text" name="materia">
+          </div>
 
-    <label for="nome">Salário</label>
-    <input type="text" name="salario">
-
-    <button type="submit">Editar</button>
-  </form>
+          <div>
+            <label for="nome">Salário:</label>
+            <input type="text" name="salario">
+          </div>
+          
+          <button type="submit">Editar</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
